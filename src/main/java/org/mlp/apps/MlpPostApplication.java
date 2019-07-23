ba1e73f2f448d7097b35a7cde3f98f3672a02acd
vvizzard <r.haritiana.z@gmail.com>
2019-07-23 11:19:52 +0300

    try to fix again

--- src/main/java/org/mlp/apps/MlpPostApplication.java
+++ src/main/java/org/mlp/apps/MlpPostApplication.java
@@ -2,12 +2,19 @@ package org.mlp.apps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MlpPostApplication {
+public class MlpPostApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MlpPostApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MlpPostApplication.class);
 	}
 
 }
